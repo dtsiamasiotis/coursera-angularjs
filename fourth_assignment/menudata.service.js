b3e82de92af15ef7d1d032cd560213c9ac748f0d
dtsiamasiotis <dtsiamasiotis@gmail.com>
2018-11-18 03:19:57 +0200

    final implementation

--- fourth_assignment/menudata.service.js
+++ fourth_assignment/menudata.service.js
@@ -1,1 +1,34 @@
- 
+(function(){
+    'use strict';
+
+    angular.module('data',[])
+        .service('MenuDataService',MenuDataService);
+
+    MenuDataService.$inject=['$http'];
+    function MenuDataService($http){
+
+        var service = this;
+
+        service.getAllCategories= function () {
+            return $http({
+                method:"GET",
+                url:("https://davids-restaurant.herokuapp.com/categories.json")
+            })
+                .then(function (response) {
+                    var responseArray = response.data;
+                    return responseArray;
+                });
+        };
+
+        service.getItemsForCategory=function(categoryShortname){
+            return $http({
+                method:"GET",
+                url:("https://davids-restaurant.herokuapp.com/menu_items.json?category="+categoryShortname)
+            })
+                .then(function(response){
+                    var responseArray = response.data.menu_items;
+                    return responseArray;
+                });
+        };
+    }
+})();
--- fourth_assignment/routes.js
+++ fourth_assignment/routes.js
@@ -1,1 +1,37 @@
- 
+(function(){
+'use strict';
+
+angular.module('MenuApp').config(RoutesConfig);
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/home.template.html'
+        })
+        .state('categories', {
+            url: '/categories',
+            templateUrl: 'templates/categories.template.html',
+            controller: 'CategoriesController as categories',
+            resolve:{
+                items:['MenuDataService', function(MenuDataService){
+                    return MenuDataService.getAllCategories();
+                }]
+                
+            }
+        })
+        .state('items',{
+            url: '/items/{categoryShortname}',
+            templateUrl: 'templates/items.template.html',
+            controller: 'ItemsController as categoryItems',
+            resolve:{
+                items: ['$stateParams', 'MenuDataService',
+                    function($stateParams,MenuDataService){
+                        return MenuDataService.getItemsForCategory($stateParams.categoryShortname);
+                    }]
+            }
+        });
+}
+})();
